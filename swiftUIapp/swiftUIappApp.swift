7c4403c694504683295f140e748112d1a2a61f95
sheff2 <shaheff01@gmail.com>
2023-12-27 14:05:44 -0500

    Initial Commit

---
+++ swiftUIapp/swiftUIappApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIappApp.swift
+//  swiftUIapp
+//
+//  Created by Shaun Heffernan on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
