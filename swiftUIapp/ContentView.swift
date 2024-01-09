6b3444757333516e3154ed3cec7f6245d3b7cfe7
sheff2 <shaheff01@gmail.com>
2024-01-09 15:53:01 -0500

    update
    -added picker wheel for tip %
    -added total check cost viewer

--- swiftUIapp/ContentView.swift
+++ swiftUIapp/ContentView.swift
@@ -15,6 +15,15 @@ struct ContentView: View {
     @FocusState private var amountIsFocused: Bool
     
     
+    var totalAmount: Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = tipSelection * (checkAmount/100)
+        let total = tipValue + checkAmount
+        return(total)
+    }
+    
     var totalPerPerson: Double{
         let peopleCount = Double(numberOfPeople + 2)
         let tipSelection = Double(tipPercentage)
@@ -45,12 +54,19 @@ struct ContentView: View {
                 
                 Section("Tip percentage"){
                     Picker("Tip percentage", selection: $tipPercentage){
-                        ForEach(tipPercentages, id: \.self){
-                            Text($0, format: .percent)
+                        //ForEach(tipPercentages, id: \.self){
+                          //  Text($0, format: .percent)
+                        //}
+                        ForEach(0..<101){
+                            Text($0, format:.percent)
+                            
                         }
                     }
-                    .pickerStyle(.segmented)
+                    .pickerStyle(.wheel)
                 
+                }
+                Section("Total amount"){
+                    Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
                 
                 Section("each person pays"){
