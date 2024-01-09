c9b85a2e1e1dbd8a6cc92057421ccbcfdb818dcc
sheff2 <shaheff01@gmail.com>
2024-01-09 15:31:40 -0500

    Check Splitter
    -simple check splitter
    -select amount of people
    -select tip

--- swiftUIapp/ContentView.swift
+++ swiftUIapp/ContentView.swift
@@ -8,19 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    
+    var totalPerPerson: Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = tipSelection * (checkAmount/100)
+        let total = tipValue + checkAmount
+        return(total/peopleCount)
+        
     }
+    
+    let tipPercentages = [0,10,15,20,25]
+    
+    var body: some View{
+        NavigationStack{
+            Form{
+                Section{
+                    TextField("amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                    
+                }
+                
+                Section("Tip percentage"){
+                    Picker("Tip percentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                
+                }
+                
+                Section("each person pays"){
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("Check Splitter")
+            .toolbar {
+                if amountIsFocused{
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
+            
+            
+        }
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
+        
